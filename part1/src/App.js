ccaf2f173c07624c6f40c8708cd3e1ae0537c3ea
pabloarancio <105462725+pabloarancio@users.noreply.github.com>
2022-10-17 19:18:01 -0300

    First Commit

---
+++ part1/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Mensaje from './Mensaje.js';
+
+const Description = () => {
+  return <p>
+    Esta es la app del curso fullstack bootcamp
+  </p>
+}
+const App = () => {
+  return (
+    <div className="App">
+      <Mensaje  message='Estamos trabajando' color='red'/>
+      <Description />
+    </div>
+  );
+}
+
+export default App;
